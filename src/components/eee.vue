f16a904f01fa64a38c65e732c225cd7fb51e64de
zhaoshuqi <852056893@qq.com>
2019-03-10 14:41:56 +0800

    加入ueditor

--- src/components/eee.vue
+++ src/components/eee.vue
@@ -1,14 +1,43 @@
 <template>
-      <div>
-          <h1>咏鹅</h1>
-          <p>鹅鹅鹅<p>
-          <p>曲项向天歌。</p>
-          <p>白毛浮丽水，</p>
-          <p>红掌拨清波。</p>
-      </div> 
+  <div>
+    <!--editor的div为富文本的承载容器-->
+    <div id="editor"></div>
+    <button type @click="gettext">点击</button>
+  </div>
 </template>
+<style>
+#editor {
+  width: 800px;
+  height: 500px;
+}
+</style>
+
 <script>
-      export default {
-            name: 'eee',
-      }
+export default {
+  data() {
+    return {
+      editor: null
+    };
+  },
+
+  mounted() {
+    // 实例化editor编辑器
+
+    this.editor = UE.getEditor("editor"); // console.log(this.editor.setContent("1223"))
+  },
+
+  methods: {
+    gettext() {
+      // 获取editor中的文本
+
+      console.log(this.editor.getContent());
+    }
+  },
+
+  destroyed() {
+    // 将editor进行销毁
+
+    this.editor.destroy();
+  }
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import '../static/Ueditor/ueditor.config.js'
+import '../static/Ueditor/ueditor.all.min.js'
+import '../static/Ueditor/lang/zh-cn/zh-cn.js'
+import '../static/Ueditor/ueditor.parse.min.js'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
