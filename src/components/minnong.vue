9d277f20a58a16128ba4b2000970af2132aee40a
zhaoshuqi <852056893@qq.com>
2019-03-12 15:16:10 +0800

    悯农模块加入引入js

--- src/components/minnong.vue
+++ src/components/minnong.vue
@@ -1,14 +1,21 @@
 <template>
-      <div>
-          <h1>悯农</h1>
-          <p>锄禾日当午，</p>
-          <p>汗滴禾下土。</p>
-          <p>谁知盘中餐，</p>
-          <p>粒粒皆辛苦。</p>
-      </div> 
+  <div>
+    <h1>悯农</h1>
+    <p>锄禾日当午，</p>
+    <p>汗滴禾下土。</p>
+    <p>谁知盘中餐，</p>
+    <p>粒粒皆辛苦。</p>
+    <button @click="diyfun">点击触发</button>
+  </div>
 </template>
 <script>
-      export default {
-            name: 'minong',
-      }
+import { myfun } from "./minnong.js";
+export default {
+  name: "minong",
+  methods: {
+    diyfun: function() {
+      myfun();
+    }
+  }
+};
 </script>
