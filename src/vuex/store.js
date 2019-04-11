95bd85000a1c8392c996cf30a7a51eb96bd0a910
zhaoshuqi <852056893@qq.com>
2019-04-11 10:13:37 +0800

    vuex研究

---
+++ src/vuex/store.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+
+/*1.state在vuex中用于存储数据*/
+var state = {
+
+  count: 7
+}
+
+/*2.mutations里面放的是方法，方法主要用于改变state里面的数据
+ */
+var mutations = {
+
+  incCount() {
+
+    ++state.count;
+  }
+}
+
+//vuex  实例化 Vuex.store
+const store = new Vuex.Store({
+  state,
+  mutations
+})
+
+
+export default store;
