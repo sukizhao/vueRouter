c8df0427e09595a0680b1ed7114b469ac9530c20
zhaoshuqi <852056893@qq.com>
2019-04-11 10:44:47 +0800

    研究emit方法

--- src/components/jingyesi/jingyesi.vue
+++ src/components/jingyesi/jingyesi.vue
@@ -1,13 +1,28 @@
 <template>
-  <div id="dateContainer">静夜思</div>
+  <div>
+    <!-- 监听子组件的showCityName事件 -->
+    <trainCity @showCityName="updateCity"></trainCity>
+  </div>
 </template>
 <script>
+import trainCity from "./trainCity.vue";
 export default {
-  name: "jingyesi",
+  name: "ingyesi",
+  components: { trainCity },
   data() {
-    return {};
+    return {
+      toCity: "北京"
+    };
   },
-  methods: {},
-  mounted() {}
+  methods: {
+    updateCity(data) {
+      //触发子组件城市选择-选择城市的事件
+      this.toCity = data.cityname; //改变了父组件的值
+      console.log("toCity:" + this.toCity);
+    }
+  }
 };
 </script>
+
+
+
---
+++ src/components/jingyesi/trainCity.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="train-city">
+    <span @click="select(`大连`)">大连</span>
+  </div>
+</template>  
+<script>
+export default {
+  name: "trainCity",
+  methods: {
+    select(val) {
+      let data = {
+        cityname: val
+      };
+      this.$emit("showCityName", data); //select事件触发后，自动触发父组件的showCityName事件
+    }
+  }
+};
+</script>  
